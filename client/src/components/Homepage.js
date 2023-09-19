c5ad5e40b6f09c1f1046cd5da374c469e7fe1f8e
Kenny_Lam <kenny.lam0802@gmail.com>
2023-09-19 00:30:03 -0600

    Homepage added

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,7 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 const Homepage = () => {
+    const[display, setDisplay] = useState([]);
+
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/allShoes")
+        .then((res)=>{
+            setDisplay(res.data);
+        })
+        .catch((err)=>console.log(err));
+    }, [])
+
     return (
         <div>
-            <h1>Homepages</h1>
+            <div>
+                <h1>Welcome to ShoeHub!</h1>
+                <Link to={'/create/new'}>Add your kicks</Link>
+            </div>
+            {
+                display?.map((shoe, index)=>(
+                    <div key={index}>
+                        <Link to={`/view/${shoe._id}/shoe`}>{shoe.shoeName}</Link>
+                    </div>
+                ))
+            }
         </div>
     )
 }

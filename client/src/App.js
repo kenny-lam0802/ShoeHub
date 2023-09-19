5581c32c720604bf3d6a19852f413149bdc52eaf
Kenny_Lam <kenny.lam0802@gmail.com>
2023-09-18 18:21:24 -0600

    add components and create form

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route,} from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Dashboard from './components/Dashboard'
+import AddShoeForm from './components/AddShoeForm';
+import EditShoe from './components/EditShoe'
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' element={<Homepage/>}/>
+          <Route path = '/view/:id/shoe' element={<Dashboard/>}/>
+          <Route path = '/create/new' element={<AddShoeForm/>}/>
+          <Route path = '/edit/:id/shoe' element = {<EditShoe/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
